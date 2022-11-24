8b6d733eb52a3e72b612015c6ad069cf78a5a86c
wdecoster <decosterwouter@gmail.com>
2022-11-24 12:59:32 +0100

    specified minimal python version  solves #15

--- setup.py
+++ setup.py
@@ -1,42 +1,44 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-exec(open('nanolyse/version.py').read())
+exec(open("nanolyse/version.py").read())
 
 setup(
-    name='NanoLyse',
+    name="NanoLyse",
     version=__version__,
-    description='Removing reads mapping to the lambda genome',
+    description="Removing reads mapping to the lambda genome",
     long_description=open(path.join(here, "README.md")).read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/wdecoster/nanolyse',
-    author='Wouter De Coster',
-    author_email='decosterwouter@gmail.com',
-    license='GPLv3',
+    url="https://github.com/wdecoster/nanolyse",
+    author="Wouter De Coster",
+    author_email="decosterwouter@gmail.com",
+    license="GPLv3",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
-    keywords='nanopore sequencing processing',
-    packages=find_packages() + ['reference'],
-    python_requires='>=3',
-    install_requires=[
-        'mappy>=2.2',
-        'biopython'],
-    package_data={'nanolyse': []},
-    package_dir={'nanolyse': 'nanolyse'},
+    keywords="nanopore sequencing processing",
+    packages=find_packages() + ["reference"],
+    python_requires=">=3.6",
+    install_requires=["mappy>=2.2", "biopython"],
+    package_data={"nanolyse": []},
+    package_dir={"nanolyse": "nanolyse"},
     include_package_data=True,
     entry_points={
-        'console_scripts': ['NanoLyse=nanolyse.NanoLyse:main', ]}
+        "console_scripts": [
+            "NanoLyse=nanolyse.NanoLyse:main",
+        ]
+    },
 )
